0bd4539d1446f15632c2079f0f4cdf97c45fa373
Keith Punches <keith.punches@gmail.com>
2024-12-06 16:45:43 -0700

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-// JavaScript to handle the page scrolling behavior or further chatbot integration
-document.addEventListener("DOMContentLoaded", function () {
-    // Scroll logic can be added here if needed
-});
